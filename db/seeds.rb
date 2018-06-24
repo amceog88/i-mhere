34aa21afcfded969e93afee72f9f874f6533019d
Siegmund <amceog88@gmail.com>
2018-06-25 00:06:24 +0800

    new things

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@
 
 puts "set admin user..."
 
-admin = User.create!(email: 'fish51412@i-mhere.com', password: 'imhere', role: 'admin')
+admin = User.create!(email: 'root@i-mhere.com', password: 'imhere', role: 'admin')
 admin.save
