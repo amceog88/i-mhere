5a7380d1c45cf888ccba48de831b52783060aeea
Siegmund <amceog88@gmail.com>
2018-07-08 22:12:16 +0800

    add new kaminari style

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -4,6 +4,24 @@ class Admin::ContactsController < ApplicationController
     @contacts = Contact.all.order(updated_at: :desc)
   end
 
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    #@contact.user = current_user
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to admin_contacts_path, notice: '新增文章成功' }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   def destroy
     @contact = Contact.find(params[:id])
